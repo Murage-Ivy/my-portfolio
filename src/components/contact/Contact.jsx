5eff8d90d86008e9fa49b4e051572e8bf7bd79ed
Murage-Ivy <ivy.murage@student.moringaschool.com>
2023-01-31 22:55:32 +0300

    Work on the contact section

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,7 +1,24 @@
 import React from "react";
-import './Contact.css'
+import "./Contact.css";
 function Contact() {
-  return <div id="contacts">Contact</div>;
+  return (
+    <div id="contacts">
+      <h3>Send a Message</h3>
+      <form className="form">
+        <label>Name</label>
+        <br />
+        <input type="text" placeholder="Enter your name" />
+        <br />
+        <label>Email</label>
+        <br />
+        <input type="email" placeholder="Enter your email" />
+        <br />
+        <label>Message</label>
+        <br />
+        <textarea placeholder="Enter Your message" rows="4" cols="100" />
+      </form>
+    </div>
+  );
 }
 
 export default Contact;
