d8fc8565058ef24be8c822378864a90bfa6c2ed9
Murage-Ivy <ivy.murage@student.moringaschool.com>
2023-01-31 11:02:54 +0300

    Create project struture

--- src/components/projects/Project.jsx
+++ src/components/projects/Project.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 import "./Project.css";
 function Project() {
-  return <div id="projects">Project</div>;
+  return (
+    <div id="projects">
+      <div className="project-card">
+        <img src="../assets/Images/restaurant.jpg" alt="restro" />
+        <p>
+          An application that enables you find
+          <br />
+          restaurants available in Nairobi
+          <br />
+          and enables you view different foods.
+        </p>
+        <button>View Project</button>
+      </div>
+
+      <div className="project-card">
+        <img src="../assets/Images/books.jpg" alt="books" />
+      </div>
+      <div className="project-card">
+        <img src="../assets/Images/farm.jpg" alt="" />
+      </div>
+      <div className="project-card"></div>
+    </div>
+  );
 }
 
 export default Project;
