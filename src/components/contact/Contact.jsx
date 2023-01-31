7b7fa2229d0861e566a66661c8757ba5082e4409
Murage-Ivy <ivy.murage@student.moringaschool.com>
2023-01-31 23:41:29 +0300

    Create and design contact form

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,21 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Contact.css";
 function Contact() {
+  const [formInfo, setFormInfo] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setFormInfo({ ...formInfo, [name]: value });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log("Submitted successfully");
+    setFormInfo({
+      name: "",
+      email: "",
+      message: "",
+    });
+  }
   return (
     <div id="contacts">
       <h3>Send a Message</h3>
-      <form className="form">
-        <label>Name</label>
-        <br />
-        <input type="text" placeholder="Enter your name" />
-        <br />
-        <label>Email</label>
-        <br />
-        <input type="email" placeholder="Enter your email" />
-        <br />
-        <label>Message</label>
-        <br />
-        <textarea placeholder="Enter Your message" rows="4" cols="100" />
+      <form className="form" onSubmit={handleSubmit}>
+        <img src="../assets/Images/contact.svg" alt="contact" />
+        <div>
+          <label>Name</label>
+          <br />
+          <input
+            type="text"
+            placeholder="Enter your name"
+            value={formInfo.name}
+            name="name"
+            onChange={handleChange}
+          />
+          <br />
+          <label>Email</label>
+          <br />
+          <input
+            type="email"
+            placeholder="Enter your email"
+            value={formInfo.email}
+            name="email"
+            onChange={handleChange}
+          />
+          <br />
+          <label>Message</label>
+          <br />
+          <textarea
+            placeholder="Enter Your message"
+            rows="4"
+            cols="100"
+            value={formInfo.message}
+            name="message"
+            onChange={handleChange}
+          />
+          <button className="submit">Submit</button>
+        </div>
       </form>
     </div>
   );
