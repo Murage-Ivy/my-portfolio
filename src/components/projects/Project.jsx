e57e40967fd70741cd8c808a730530f9e6d7ee90
Murage-Ivy <ivy.murage@student.moringaschool.com>
2023-01-31 21:42:29 +0300

    Refactor code for the project section

--- src/components/projects/Project.jsx
+++ src/components/projects/Project.jsx
@@ -3,28 +3,66 @@ import "./Project.css";
 function Project() {
   return (
     <div id="projects">
-      <div className="project-card">
-        <img src="../assets/Images/restaurant.jpg" alt="restro" />
-        <p>
-          An application that enables you find
-          <br />
-          restaurants available in Nairobi
-          <br />
-          and enables you view different foods.
-        </p>
-        <div className="primary-actions-btn">
-          <button>View Project</button>
-          <button>View Github</button>
+      <h2>Projects</h2>
+      <div className="projects">
+        <div className="project-card">
+          <div className="img">
+            <img src="../assets/Images/restaurant.jpg" alt="restro" />
+          </div>
+          <p>
+            An application that enables you find
+            <br />
+            restaurants available in Nairobi
+            <br />
+            and enables you view different foods.
+          </p>
+          <div className="primary-actions-btn">
+            <button>View Project</button>
+            <button>View Github</button>
+          </div>
+        </div>
+
+        <div className="project-card">
+          <div className="img">
+            <img src="../assets/Images/books.jpg" alt="books" />
+          </div>
+          <p>
+            An application that enables readers who enjoy african books to share
+            the their interesting reads and also find a book to read
+          </p>
+          <div className="primary-actions-btn">
+            <button>View Project</button>
+            <button>View Github</button>
+          </div>
+        </div>
+        <div className="project-card">
+          <div className="img">
+            <img src="../assets/Images/farm.jpg" alt="" />
+          </div>
+
+          <p>
+            This is a social platform that connects farmers from across the
+            different religion.
+          </p>
+          <div className="primary-actions-btn">
+            <button>View Project</button>
+            <button>View Github</button>
+          </div>
+        </div>
+        <div className="project-card">
+          <div className="img">
+            <img src="../assets/Images/handyman.jpg" alt="handyman" />
+          </div>
+          <p>
+            This is an application that connects handymen to people who are need
+            of handymen services
+          </p>
+          <div className="primary-actions-btn">
+            <button>View Project</button>
+            <button>View Github</button>
+          </div>
         </div>
       </div>
-
-      <div className="project-card">
-        <img src="../assets/Images/books.jpg" alt="books" />
-      </div>
-      <div className="project-card">
-        <img src="../assets/Images/farm.jpg" alt="" />
-      </div>
-      <div className="project-card"></div>
     </div>
   );
 }
